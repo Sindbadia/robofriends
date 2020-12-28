8b2e0d4c7d5e496994a88bdb7324e8f42f7e17a4
Captain IR <52345388+Captain-IR@users.noreply.github.com>
2020-12-28 22:07:13 +0200

    Added componentDidMount and fetch

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 import './App.css'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import { robots } from './robots'
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots,
+			robots: [],
 			searchfield: '',
 		}
+	}
+
+	async componentDidMount() {
+		const res = await fetch('https://jsonplaceholder.typicode.com/users')
+		const users = await res.json()
+		this.setState({ robots: users })
 	}
 
 	onSearchChange = event => {
@@ -22,13 +27,17 @@ class App extends Component {
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 		})
 
-		return (
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<CardList robots={filteredRobots} />
-			</div>
-		)
+		if (!this.state.robots.length) {
+			return <h1>Loading</h1>
+		} else {
+			return (
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<CardList robots={filteredRobots} />
+				</div>
+			)
+		}
 	}
 }
 
